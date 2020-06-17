9af26df064d08228496a75ff6038d380160f9b3b
Jeremy Diaz <jedi4302@colorado.edu>
2020-06-17 13:23:48 -0400

    Script to download data given lat, lon, and volcano name

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+# Downloads all ASTER imagery from beginning of data record
+# to May 1, 2020 for specified volcano
+# THINGS TO SPECIFY
+# 	1. coord_1 - lon of approx geographic midpoint determined by Google Maps satellite imagery
+#	2. coord_2 - lat of approx geographic midpoint determined by Google Maps satellite imagery
+#	3. volcano_name - directory within the data folder where data will be stored
+# MAKE SURE YOU ARE IN THE MAIN PROJECT DIRECTORY (i.e. VolcanoesIR)
+./Daac2Disk_ubuntu18 --shortname AST_L1T --versionid 003 --browse --begin 1999-12-18 --end 2020-05-1 --bbox coord_1 13.604800 40.662474 13.604800 --outputdir data/volcano_name/

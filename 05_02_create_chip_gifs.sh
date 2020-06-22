02c09922899a3849610e672616972da600382481
Jeremy Diaz <jedi4302@colorado.edu>
2020-06-22 11:23:03 -0400

    Added memory issue note for gif creation

--- 05_02_create_chip_gifs.sh
+++ 05_02_create_chip_gifs.sh
@@ -5,4 +5,9 @@
 
 # Change "volcano" to the name of the specific one
 # Make sure that you are within the "temp_gif_image_holder" directory
+
+# CAUTION
+# If you run into memory issues you can edit the ImageMagick limits
+# on Ubuntu via sudo gedit /etc/ImageMagick-6/policy.xml
+# I changed the memory limit to 4 gigabytes
 convert -delay 25 *.png volcano.gif

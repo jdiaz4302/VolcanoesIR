551e0494d8206fa47c8c5eb16dbf22213a1b9009
Jeremy Diaz <jedi4302@colorado.edu>
2020-08-20 15:18:42 -0400

    Corrected input parameters to be generic variable, not specific values

--- 01_download_data.sh
+++ 01_download_data.sh
@@ -5,4 +5,4 @@
 #	2. coord_2 - lat of approx geographic midpoint determined by Google Maps satellite imagery
 #	3. volcano_name - directory within the data folder where data will be stored
 # MAKE SURE YOU ARE IN THE MAIN PROJECT DIRECTORY (i.e. VolcanoesIR)
-./Daac2Disk_ubuntu18 --shortname AST_L1T --versionid 003 --browse --begin 1999-12-18 --end 2020-05-1 --bbox coord_1 13.604800 40.662474 13.604800 --outputdir data/volcano_name/
+./Daac2Disk_ubuntu18 --shortname AST_L1T --versionid 003 --browse --begin 1999-12-18 --end 2020-05-1 --bbox coord_1 coord2 coord1 coord2 --outputdir data/volcano_name/

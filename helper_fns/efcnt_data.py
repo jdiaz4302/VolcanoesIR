926be336a8ef94f0623a6eb7b666bea518cfbf0c
Jeremy Diaz <jedi4302@colorado.edu>
2020-08-28 16:27:29 -0400

    storing intermed file for local editing

--- helper_fns/efcnt_data.py
+++ helper_fns/efcnt_data.py
@@ -2,23 +2,20 @@
 
 
 
-# Assumptions:
-#   1. That x_train/valid, t_train/valid, and y_train/valid
-#      are defined in the environment
-
-
 # Dependencies
 from torch.utils import data
 
 
 # Defining data set/loading classes for efficient
 # PyTorch computing
-# ...for the training set
-class train_Dataset(data.Dataset):
+class efficient_Dataset(data.Dataset):
 	'Characterizes a dataset for PyTorch'
-	def __init__(self, data_indices):
+	def __init__(self, data_indices, x, t, y):
 		'Initialization'
 		self.data_indices = data_indices
+                self.x = x
+                self.t = t
+                self.y = y
 	def __len__(self):
 		'Denotes the total number of samples'
 		return len(self.data_indices)
@@ -27,29 +24,8 @@ class train_Dataset(data.Dataset):
 		# Select sample
 		IDs = self.data_indices[index]
 		# Load data and get label
-		curr_x = x_train[IDs, :, :, :, :]
-		curr_t = t_train[IDs, :, :, :, :]
-		curr_y = y_train[IDs, :, :, :, :]
+		curr_x = x[IDs, :, :, :, :]
+		curr_t = t[IDs, :, :, :, :]
+		curr_y = y[IDs, :, :, :, :]
 		#return X, y
-		return(curr_x, curr_t, curr_y)
-
-
-# ...for the validation set
-class validation_Dataset(data.Dataset):
-	'Characterizes a dataset for PyTorch'
-	def __init__(self, data_indices):
-		'Initialization'
-		self.data_indices = data_indices
-	def __len__(self):
-		'Denotes the total number of samples'
-		return len(self.data_indices)
-	def __getitem__(self, index):
-		'Generates one sample of data'
-		# Select sample
-		IDs = self.data_indices[index]
-		# Load data and get label
-		curr_x = x_valid[IDs, :, :, :, :]
-		curr_t = t_valid[IDs, :, :, :, :]
-		curr_y = y_valid[IDs, :, :, :, :]
-		#return X, y
-		return(curr_x, curr_t, curr_y)
+        	return(curr_x, curr_t, curr_y)

413cfb3f610069fbec23154d14976a42465e595f
Jeremy Diaz <jedi4302@colorado.edu>
2020-09-08 10:20:38 -0400

    Added transformation parameter saving functionality

--- helper_fns/processing.py
+++ helper_fns/processing.py
@@ -5,14 +5,17 @@
 import numpy as np
 
 
-def scale_and_remove_na(np_array):
+def scale_and_remove_na(np_array, parameter_storage, index):
     
-    # Getting and storing parameters
+    # Getting parameters for transformation
     min_val = np.nanmin(np_array)
     max_val = np.nanmax(np_array)
+    # Storing those parameters for inversion transformation
+    parameter_storage[0, index] = min_val
+    parameter_storage[1, index] = max_val
     # Scaling between 0 and 1
     np_array_scaled = (np_array - min_val) / (max_val - min_val)
     # Replacing nan with 0
     np_array_scaled_no_nans = np.nan_to_num(np_array_scaled)
     
-    return(np_array_scaled_no_nans)
+    return(np_array_scaled_no_nans, parameter_storage)

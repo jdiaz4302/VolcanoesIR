b7e4ae8d39742ca6577cfe2b7587a44cb70937e0
Jeremy Diaz <jedi4302@colorado.edu>
2020-09-23 11:02:54 -0400

    Vector time to scalar; removed printing

---
+++ models/TimeAwareLSTM.py
@@ -0,0 +1,77 @@
+# Adapted from: https://github.com/keitakurita/Practical_NLP_in_PyTorch/blob/master/deep_dives/lstm_from_scratch.ipynb
+import torch
+import torch.nn as nn
+from enum import IntEnum
+
+class Dim(IntEnum):
+    batch = 0
+    seq = 1
+    feature = 2
+
+
+class TimeAwareLSTM(nn.Module):
+    def __init__(self, input_sz: int, hidden_sz: int):
+        super().__init__()
+        self.input_sz = input_sz
+        self.hidden_size = hidden_sz
+        self.weights_x = nn.Parameter(torch.Tensor(input_sz, hidden_sz * 4))
+        self.weights_h = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
+        # Additionally, weights for adjusting memory by time differences
+        self.weights_t = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        # Adapted for i, f, o, c and c-new/adjusted
+        self.bias = nn.Parameter(torch.Tensor(hidden_sz * 5))
+        self.init_weights()
+    
+    def init_weights(self):
+        for p in self.parameters():
+            if p.data.ndimension() >= 2:
+                nn.init.xavier_uniform_(p.data)
+            else:
+                nn.init.zeros_(p.data)
+        
+    def forward(self, x, TimeDiff, init_states=None):
+        """Assumes x is of shape (batch, sequence, feature)"""
+        bs, seq_sz, _ = x.size()
+        hidden_seq = []
+        if init_states is None:
+            h_t, c_t = (torch.zeros(self.hidden_size).to(x.device), 
+                        torch.zeros(self.hidden_size).to(x.device))
+        else:
+            h_t, c_t = init_states
+        
+        HS = self.hidden_size
+        for t in range(seq_sz):
+            x_t = x[:, t, :]
+            TimeDiff_t = TimeDiff[:, t, :]
+            # batch the computations into a single matrix multiplication
+            # And apply all TimeAwareLSTM equations
+            # Adjusting previous memory
+            # Short term memory
+            c_s_t_minus_1 = torch.tanh(c_t @ self.weights_t + self.bias[:HS])
+            # Discounted short term memory, scalar assumption for TimeDiff
+            c_hat_s_t_minus_1 = c_s_t_minus_1 * (-1 * torch.tanh(TimeDiff_t))
+            # Long-term memory
+            c_t_t_minus_1 = c_t - c_s_t_minus_1
+            # Adjusted previous memory
+            c_star_t_minus_1 = c_t_t_minus_1 + c_hat_s_t_minus_1
+            # Input gate
+            i_t = x_t @ self.weights_x[:, :HS] + h_t @ self.weights_h[:, :HS] + self.bias[HS:HS*2]
+            i_t = torch.sigmoid(i_t)
+            # Forget gate
+            f_t = x_t @ self.weights_x[:, HS:HS*2] + h_t @ self.weights_h[:, HS:HS*2] + self.bias[HS*2:HS*3]
+            f_t = torch.sigmoid(f_t)
+            # Output gate
+            o_t = x_t @ self.weights_x[:, HS*2:HS*3] + h_t @ self.weights_h[:, HS*2:HS*3] + self.bias[HS*3:HS*4]
+            o_t = torch.sigmoid(o_t)
+            # Candidate memory
+            c_tilde = x_t @ self.weights_x[:, HS*3:] + h_t @ self.weights_h[:, HS*3:] + self.bias[HS*4:]
+            c_tilde = torch.tanh(c_tilde)
+            # Current memory
+            c_t = (f_t * c_star_t_minus_1) + (i_t * c_tilde)
+            # Current hidden state
+            h_t = o_t + torch.tanh(c_t)
+            hidden_seq.append(h_t.unsqueeze(Dim.batch))
+        hidden_seq = torch.cat(hidden_seq, dim=Dim.batch)
+        # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
+        hidden_seq = hidden_seq.transpose(Dim.batch, Dim.seq).contiguous()
+        return hidden_seq, (h_t, c_t)

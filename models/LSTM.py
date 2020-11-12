ef695cd43e25612ccf9febc93a96f2ff0250b41e
Jeremy Diaz <jedi4302@colorado.edu>
2020-11-12 10:56:28 -0500

    Adding script-compatible, stackable LSTM

--- models/LSTM.py
+++ models/LSTM.py
@@ -9,7 +9,7 @@ class Dim(IntEnum):
     feature = 2
 
 # Simple LSTM made from scratch
-class OptimizedLSTM(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int, GPU, input_size=False, num_layers=False):
         super().__init__()
         self.input_dim = input_dim
@@ -59,3 +59,59 @@ class OptimizedLSTM(nn.Module):
         # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
         hidden_seq = hidden_seq.transpose(Dim.batch, Dim.seq).contiguous()
         return hidden_seq, (h_t, c_t)
+    
+# Stacking the base LSTM class for a deeper network
+class StackedLSTM(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, GPU, input_size=False, num_layers=False):
+        """Simply stacking the simple TimeLSTM for multilayer model"""
+        super(StackedLSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.GPU = GPU
+        self.input_size = input_size # image h and w, relic from/for spatial models
+        self.num_layers = num_layers # also relic
+        # Wanting more/less than 4 layers will require manual editting
+        
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i-1]
+
+            cell_list.append(LSTM(input_dim=cur_input_dim,
+                                  hidden_dim=self.hidden_dim[i],
+                                  GPU=self.GPU))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, x):
+
+        layer_output_list = []
+        last_state_list   = []
+
+        seq_len = x.size(1)
+
+        for layer_idx in range(self.num_layers):
+            output_inner = []
+            for k in range(seq_len):
+                if k == 0 and layer_idx == 0:
+                    h, c = self.cell_list[layer_idx](x[:, [k], :])
+                elif k == 0 and layer_idx != 0:
+                    h, c = self.cell_list[layer_idx](x[:, k, :])
+                # If not the first ele. in seq., use hidden state
+                elif k != 0 and layer_idx != 0:
+                    h, c = self.cell_list[layer_idx](x[:, k, :],
+                                                     init_states=[c[0],c[1]])
+                elif k != 0 and layer_idx == 0:
+                    h, c = self.cell_list[layer_idx](x[:, [k], :],
+                                                     init_states=[c[0],c[1]])
+                output_inner.append(h)
+            layer_output = torch.stack(output_inner, dim=1)
+            layer_output.shape
+            x = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])
+
+        layer_output_list = layer_output_list[-1:]
+        last_state_list   = last_state_list[-1:]
+
+        return layer_output_list, last_state_list

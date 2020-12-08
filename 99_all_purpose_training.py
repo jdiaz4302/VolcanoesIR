cc54bc67eb93269202c36b192a2c09e6e74634bd
Jeremy Diaz <jedi4302@colorado.edu>
2020-12-08 12:22:51 -0500

    Correct indices used to store valid/test set time diff

--- 99_all_purpose_training.py
+++ 99_all_purpose_training.py
@@ -191,13 +191,13 @@ for vol in volcanoes:
 				dates_i_plus_1 = formatted_dates[(i - num_input_scenes + 1):(i + 1)]
 				dates_i = formatted_dates[(i - num_input_scenes):i]
 				for j in range(len(dates_i_plus_1)):
-					time_differences_valid[i - num_input_scenes, j] = (dates_i_plus_1[j] - dates_i[j]).days
+					time_differences_valid[i - num_input_scenes - train_n, j] = (dates_i_plus_1[j] - dates_i[j]).days
 			# While Time-Aware LSTM uses backwards-time interval
 			else:
 				dates_i = formatted_dates[(i - num_input_scenes):i]
 				dates_i_minus_1 = formatted_dates[(i - num_input_scenes - 1):(i - 1)]
 				for j in range(len(dates_i)):
-					time_differences_valid[i - num_input_scenes, j] = (dates_i[j] - dates_i_minus_1[j]).days
+					time_differences_valid[i - num_input_scenes - train_n, j] = (dates_i[j] - dates_i_minus_1[j]).days
 		else:
 			# Store the image data
 			x_scenes_test[i - train_n - out_n - num_input_scenes, :, :, :, :] = volcano_scenes[(i - num_input_scenes):i, :, :, :]
@@ -211,13 +211,13 @@ for vol in volcanoes:
 				dates_i_plus_1 = formatted_dates[(i - num_input_scenes + 1):(i + 1)]
 				dates_i = formatted_dates[(i - num_input_scenes):i]
 				for j in range(len(dates_i_plus_1)):
-					time_differences_test[i - num_input_scenes, j] = (dates_i_plus_1[j] - dates_i[j]).days
+					time_differences_test[i - num_input_scenes - train_n - out_n, j] = (dates_i_plus_1[j] - dates_i[j]).days
 			# While Time-Aware LSTM uses backwards-time interval
 			else:
 				dates_i = formatted_dates[(i - num_input_scenes):i]
 				dates_i_minus_1 = formatted_dates[(i - num_input_scenes - 1):(i - 1)]
 				for j in range(len(dates_i)):
-					time_differences_test[i - num_input_scenes, j] = (dates_i[j] - dates_i_minus_1[j]).days
+					time_differences_test[i - num_input_scenes - train_n - out_n, j] = (dates_i[j] - dates_i_minus_1[j]).days
 	if count == 0:
 		x_train = x_scenes_train
 		t_train = time_differences_train

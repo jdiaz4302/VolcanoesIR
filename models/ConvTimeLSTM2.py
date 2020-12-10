056dcb1de63b279276dad9a37df843dfa3e5fde3
Jeremy Diaz <jedi4302@colorado.edu>
2020-12-09 21:19:31 -0500

    Readded accidently removed t-inclusion of ConvLSTM+TimeLSTM o-gate

--- models/ConvTimeLSTM2.py
+++ models/ConvTimeLSTM2.py
@@ -158,7 +158,7 @@ class ConvTime_LSTM2Cell(nn.Module):
          
         ## The output gate ##
         ## Running the output gate convolution ##
-        o_conv_output = self.o_conv(x_h_combined)
+        o_conv_output = self.o_conv(x_h_t_combined)
         ## Running the output LSTM gate equations ##
         o_m = torch.sigmoid(o_conv_output)
         

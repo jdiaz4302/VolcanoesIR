554c366e6883d45337d40adcd37a71583e985e2b
Jeremy Diaz <jedi4302@colorado.edu>
2020-12-16 18:59:34 -0500

    Corrected AR implementation

--- models/AR.py
+++ models/AR.py
@@ -1,11 +1,17 @@
 import torch
+import torch.nn as nn
 
 class AR(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, GPU, input_size, num_layers, kernel_size=(3, 3),
                  batch_first=True, bias=False, return_all_layers=False):
         super(AR, self).__init__()
-        self.linear = torch.nn.Linear(input_dim, hidden_dim[0], bias=False)
+        # Hard-coded for 'all' only
+        print("WARNING: this model assumes training_data_set = 'all' by using 6 previous scenes")
+        self.AR_coefs = nn.Parameter(torch.randn(6))
 
     def forward(self, x):
-        out = self.linear(x)
+        out = torch.zeros(x.shape)
+        for p in range(len(self.AR_coefs)):
+            out[:, p, :] = self.AR_coefs[p]*x[:, p, :]
+        out = torch.cumsum(out, dim = 1)
         return out

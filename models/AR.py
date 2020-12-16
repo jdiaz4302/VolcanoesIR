cd399b489257be7e451f0e646e72636d826be5e2
Jeremy Diaz <jedi4302@colorado.edu>
2020-12-16 11:46:19 -0500

    Added simple autoregressive model

---
+++ models/AR.py
@@ -0,0 +1,11 @@
+import torch
+
+class AR(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, GPU, input_size, num_layers, kernel_size=(3, 3),
+                 batch_first=True, bias=True, return_all_layers=False):
+        super(AR, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, hidden_dim)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out

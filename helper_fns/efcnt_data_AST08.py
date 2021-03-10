58d7df07a5b0630b24d0725c15fd68ba0aa390b7
Jeremy Diaz <jedi4302@colorado.edu>
2021-03-10 17:40:51 -0500

    New helper function, same as previous but changed dimensionality assumption

---
+++ helper_fns/efcnt_data_AST08.py
@@ -0,0 +1,31 @@
+
+
+
+
+# Dependencies
+from torch.utils import data
+
+
+# Defining data set/loading classes for efficient
+# PyTorch computing
+class efficient_Dataset(data.Dataset):
+	'Characterizes a dataset for PyTorch'
+	def __init__(self, data_indices, x, t, y):
+		'Initialization'
+		self.data_indices = data_indices
+		self.x = x
+		self.t = t
+		self.y = y
+	def __len__(self):
+		'Denotes the total number of samples'
+		return len(self.data_indices)
+	def __getitem__(self, index):
+		'Generates one sample of data'
+		# Select sample
+		IDs = self.data_indices[index]
+		# Load data and get label
+		curr_x = self.x[IDs, :, :, :]
+		curr_t = self.t[IDs, :, :, :]
+		curr_y = self.y[IDs, :, :, :]
+		#return X, y
+		return(curr_x, curr_t, curr_y)

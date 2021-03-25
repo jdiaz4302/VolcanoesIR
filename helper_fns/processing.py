cdcf38e9c7aa80dcca964d5c560a51aad93438a3
Jeremy Diaz <jedi4302@colorado.edu>
2021-03-25 12:03:44 -0400

    Updated helper funct. to not remove NAs

--- helper_fns/processing.py
+++ helper_fns/processing.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 def scale_and_remove_na(np_array, parameter_storage, index):
+
+    print('Functionality deprecation: scale_and_remove_na no longer removes \
+           na values because they should now be dealed with in other stages')
     
     # Getting parameters for transformation
     min_val = np.nanmin(np_array)
@@ -16,6 +19,6 @@ def scale_and_remove_na(np_array, parameter_storage, index):
     # Scaling between 0 and 1
     np_array_scaled = (np_array - min_val) / (max_val - min_val)
     # Replacing nan with 0
-    np_array_scaled_no_nans = np.nan_to_num(np_array_scaled)
+    #np_array_scaled_no_nans = np.nan_to_num(np_array_scaled)
     
-    return(np_array_scaled_no_nans, parameter_storage)
+    return(np_array_scaled, parameter_storage)

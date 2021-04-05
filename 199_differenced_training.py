7025a97b9c1227596bc7f8afbdcde43fcb19ccc3
Jeremy Diaz <jedi4302@colorado.edu>
2021-04-05 11:49:19 -0400

    Corrected indexing dimensions

--- 199_differenced_training.py
+++ 199_differenced_training.py
@@ -340,34 +340,34 @@ for i in range(len(y_test)):
 
 # First differencing - which removes the first values
 # Train
-x_train_differenced = x_train[1:len(x_train), :, :, :, :] - x_train[0:len(x_train)-1, :, :, :, :]
-y_train_differenced = y_train[1:len(y_train), :, :, :, :] - y_train[0:len(y_train)-1, :, :, :, :]
+x_train_differenced = x_train[1:len(x_train), :, :, :] - x_train[0:len(x_train)-1, :, :, :]
+y_train_differenced = y_train[1:len(y_train), :, :, :] - y_train[0:len(y_train)-1, :, :, :]
 # Valid
-x_valid_differenced = x_valid[1:len(x_valid), :, :, :, :] - x_valid[0:len(x_valid)-1, :, :, :, :]
-y_valid_differenced = y_valid[1:len(y_valid), :, :, :, :] - y_valid[0:len(y_valid)-1, :, :, :, :]
+x_valid_differenced = x_valid[1:len(x_valid), :, :, :] - x_valid[0:len(x_valid)-1, :, :, :]
+y_valid_differenced = y_valid[1:len(y_valid), :, :, :] - y_valid[0:len(y_valid)-1, :, :, :]
 # Test
-x_test_differenced = x_test[1:len(x_test), :, :, :, :] - x_test[0:len(x_test)-1, :, :, :, :]
-y_test_differenced = y_test[1:len(y_test), :, :, :, :] - y_test[0:len(y_test)-1, :, :, :, :]
+x_test_differenced = x_test[1:len(x_test), :, :, :] - x_test[0:len(x_test)-1, :, :, :]
+y_test_differenced = y_test[1:len(y_test), :, :, :] - y_test[0:len(y_test)-1, :, :, :]
 
 # Reproviding the first value
 # Train
 #   X needs more work, has to draw from volcano_scenes which cycles through directories and may have NAs
 #   Y suffers as a result of X
-#       first_train_differenced_y = y_train[[0], :, :, :, :] - x_train[[-1], [-1], :, :, :]
+#       first_train_differenced_y = y_train[[0], :, :, :] - x_train[[-1], [-1], :, :, :]
 #       y_train_differenced = np.concatenate([first_train_differenced_y, y_train_differenced], axis = 0)
 # Validation
 #   X
-first_valid_differenced = x_valid[[0], :, :, :, :] - x_train[[-1], :, :, :, :]
+first_valid_differenced = x_valid[[0], :, :, :] - x_train[[-1], :, :, :]
 x_valid_differenced = np.concatenate([first_valid_differenced, x_valid_differenced], axis = 0)
 #   Y
-first_valid_differenced_y = y_valid[[0], :, :, :, :] - x_valid[[-1], [-1], :, :, :]
+first_valid_differenced_y = y_valid[[0], :, :, :] - x_valid[[-1], [-1], :, :, :]
 y_valid_differenced = np.concatenate([first_valid_differenced_y, y_valid_differenced], axis = 0)
 # Test
 #   X
-first_test_differenced = x_test[[0], :, :, :, :] - x_valid[[-1], :, :, :, :]
+first_test_differenced = x_test[[0], :, :, :] - x_valid[[-1], :, :, :]
 x_test_differenced = np.concatenate([first_test_differenced, x_test_differenced], axis = 0)
 #   Y
-first_test_differenced_y = y_test[[0], :, :, :, :] - x_test[[-1], [-1], :, :, :]
+first_test_differenced_y = y_test[[0], :, :, :] - x_test[[-1], [-1], :, :, :]
 y_test_differenced = np.concatenate([first_test_differenced_y, y_test_differenced], axis = 0)
 # Replacing state values with differenced values
 x_train_og = x_train

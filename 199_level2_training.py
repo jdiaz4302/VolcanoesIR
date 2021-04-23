6eb9d5887d490f0498fc84a9fdecb0ed5420c0af
Jeremy Diaz <jedi4302@colorado.edu>
2021-04-23 16:10:54 -0400

    More elaborate batch size determination based on model size

--- 199_level2_training.py
+++ 199_level2_training.py
@@ -125,9 +125,25 @@ assert((training_data_set in volcanoes) or (training_data_set == "all"))
 
 # Training parameters
 # This needs to actually be variable, will do with later exploration
-### batch_size_dict = {'AR':84, 'Identity':84, 'LSTM':84, 'TimeLSTM':46, 'TimeAwareLSTM':70, 'ConvLSTM':24, 'ConvTimeLSTM':32, 'ConvTimeAwareLSTM':60, 'ConvTimeLSTMUnet':32}
+assert(hidden_dim_ls[0] in [2000, 1028, 128, 64])
+if hidden_dim_ls[0] == 2000: # [2000, 2000, 1]
+    assert(model_selection in ['AR', 'Identity', 'LSTM', 'TimeLSTM', 'TimeAwareLSTM'])
+    batch_size_dict = {'AR':84, 'Identity':84, 'LSTM':4, 'TimeLSTM':2, 'TimeAwareLSTM':2}
+    batch_size = batch_size_dict[model_selection]
+if hidden_dim_ls[0] == 1028: # [1028, 512, 1]
+    assert(model_selection in ['LSTM', 'TimeLSTM', 'TimeAwareLSTM'])
+    batch_size_dict = {'LSTM':12, 'TimeLSTM':6, 'TimeAwareLSTM':10}
+    batch_size = batch_size_dict[model_selection]
+if hidden_dim_ls[0] == 128: # [128, 1]
+    assert(model_selection in ['LSTM', 'TimeLSTM', 'TimeAwareLSTM'])
+    batch_size_dict = {'LSTM':128, 'TimeLSTM':80, 'TimeAwareLSTM':114}
+    batch_size = batch_size_dict[model_selection]
+if hidden_dim_ls[0] == 64: # [64, 64, 1]; [64, 128, 64, 1] for Unet
+    assert(model_selection in ['ConvLSTM', 'ConvTimeLSTM', 'ConvTimeAwareLSTM', 'ConvTimeLSTMUnet'])
+    batch_size_dict = {'ConvLSTM':108, 'ConvTimeLSTM':76, 'ConvTimeAwareLSTM':124, 'ConvTimeLSTMUnet':36}
+    batch_size = batch_size_dict[model_selection]
 lag_dict = {"all":6, "ErtaAle":9, "Kilauea":10, "Masaya":3, "Nyamuragira":3, "Nyiragongo":3, "Pacaya":4, "Puuoo":8}
-batch_size = 4 # Need something to work for all configurations
+
 num_input_scenes = lag_dict[training_data_set]
 train_percent = 0.70
 out_samp_perc = 0.15
